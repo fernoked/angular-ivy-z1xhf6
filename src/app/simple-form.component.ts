8bcb32d87310ed97a9e11687a84337e80ce045d8
fernoked <carlosmartingalindo@yahoo.es>
2022-01-04 14:36:33 +0100

    4 files modified

--- src/app/simple-form.component.ts
+++ src/app/simple-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Title } from './title.model';
 import { TitleService } from './title.service';
 
 @Component({
@@ -8,14 +11,49 @@ import { TitleService } from './title.service';
   styleUrls: ['./simple-form.component.css'],
 })
 export class SimpleFormComponent implements OnInit, OnDestroy {
-  titles$: Observable<string>;
+  titles: Title[];
   titleSubscription: Subscription;
+  terms: boolean = false;
+  testFormGroup = new FormGroup({
+    title: new FormControl(),
+    firstName: new FormControl(''),
+    lastName: new FormControl('', [Validators.required]),
+    terms: new FormControl(false),
+  });
+  errorMessage: string;
   constructor(private titlesService: TitleService) {
-    this.titleSubscription = this.titlesService.getTitles().subscribe(() => {});
+    this.titleSubscription = this.titlesService
+      .getTitles()
+      .pipe(map((data) => this.removedByBeChickAndGetDefaultAndSort(data)))
+      .subscribe((x) => {
+        this.titles = x;
+      });
   }
-  ngOnInit() {}
+  ngOnInit() {
+    this.errorMessage = '* Last name is mandatory';
+  }
 
   ngOnDestroy() {
     this.titleSubscription.unsubscribe();
+  }
+
+  removedByBeChickAndGetDefaultAndSort(val: Title[]) {
+    const defaultTitle: Title = val.find((title) => title.isDefault === true);
+    this.testFormGroup.controls['title'].setValue(defaultTitle.name);
+    return val
+      .filter((title) => title.name !== '!')
+      .sort((a, b) => {
+        return a.name < b.name ? -1 : 1;
+      });
+  }
+
+  formSubmit() {
+    if (this.testFormGroup.valid) {
+      console.log(
+        `${this.testFormGroup.controls['title'].value} ${this.testFormGroup.controls['firstName'].value} ${this.testFormGroup.controls['lastName'].value} `
+      );
+    } else {
+      this.testFormGroup.get('lastName').markAsTouched();
+    }
   }
 }
